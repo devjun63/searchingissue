2fbe33c666097c232ff024631e72e60a8bcd08ce
devjun63 <59526539+devjun63@users.noreply.github.com>
2021-08-25 17:02:49 +0900

    Create Project

---
+++ src/main/java/com/devjun/searchingissue/SearchingissueApplication.java
@@ -0,0 +1,13 @@
+package com.devjun.searchingissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchingissueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchingissueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjun/searchingissue/SearchingissueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjun.searchingissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchingissueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
